101882534755bf50e0ed75a142f60e1c9d408200
mintchocoicecream <sxxhnjng@gmail.com>
2022-03-04 17:15:30 +0900

    #1 SetUp Firebase Setup

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDTEPTrjQLQQQabrQVMx6PL1rqWugw9mOs",
+    authDomain: "mintwitter-48f72.firebaseapp.com",
+    projectId: "mintwitter-48f72",
+    storageBucket: "mintwitter-48f72.appspot.com",
+    messagingSenderId: "139834263517",
+    appId: "1:139834263517:web:9983a4c351cbc97705d834"
+  };
+
+export default firebase.initializeApp(firebaseConfig);
