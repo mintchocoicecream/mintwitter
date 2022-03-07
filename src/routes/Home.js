7f0e0fa22b0088d1f6b9b266a57adb01141921c2
mintchocoicecream <sxxhnjng@gmail.com>
2022-03-07 20:34:05 +0900

    #3 Nweeting Getting the Nweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { dbService } from "fbase";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, getDocs, query } from "firebase/firestore";
 
 const Home = () => {
     const [mintweet, setMintweet] = useState("");
+    const [mintweets, setMintweets] = useState([]);
+    const getMintweets = async () => {
+        const q = query(collection(dbService, "mintweets"));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach( (doc) => {
+            const mintweetObj = {
+            ...doc.data(),
+            id: doc.id,
+            }
+            setMintweets(prev => [mintweetObj, ...prev]);
+            });
+        };
+    useEffect(() => {
+        getMintweets();
+    }, [])
     const onSubmit = async(e) => {
         e.preventDefault();
         try{
@@ -21,12 +36,21 @@ const Home = () => {
         const {target: {value}} = event;
         setMintweet(value);
     }
+    console.log(mintweets);
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input value={mintweet} type="text" placeholder="What's on your min?" onChange={onChange} maxLength={120}/>
                 <input type="submit" value="Mintweet" />
             </form>
+            <div>
+                {mintweets.map( (mintweet) => (
+                    <div key={mintweet.id}>
+                        <h4>{mintweet.mintweet}</h4>
+                    </div>
+                    )
+                )}
+            </div>
         </div>
     )
 }
