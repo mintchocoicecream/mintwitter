f98f1e7154cd8caf3ac8546bbbd9c0b422efaa80
mintchocoicecream <sxxhnjng@gmail.com>
2022-03-07 17:10:44 +0900

    #3 Nweeting Form and Databse Setup

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+    const [mintweet, setMintweet] = useState("");
+    const onSubmit = (event) => {
+        event.preventDefault(); 
+    };
+    const onChange = (event) => {
+        const {target: {value}} = event;
+        setMintweet(value);
+    }
+    return (
+        <div>
+            <form>
+                <input value={mintweet} type="text" placeholder="What's on your min?" onChange={onChange} maxLength={120}/>
+                <input type="submit" value="Mintweet" />
+            </form>
+        </div>
+    )
+}
 
 export default Home;
