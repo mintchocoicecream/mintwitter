f7129510ec3620ccca24d7fc8e61d4e8bad7fcd2
mintchocoicecream <sxxhnjng@gmail.com>
2022-03-08 16:36:58 +0900

    #4 File Upload. Preview Images

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,6 +6,7 @@ import Mintweet from "components/Mintweet";
 const Home = ({userObj}) => {
     const [mintweet, setMintweet] = useState("");
     const [mintweets, setMintweets] = useState([]);
+    const [attachment, setAttachment] = useState();
     useEffect(() => {
         const q = query(
             collection(dbService, "mintweets"),
@@ -38,11 +39,36 @@ const Home = ({userObj}) => {
         const {target: {value}} = event;
         setMintweet(value);
     }
+    const onFileChange = (event) => {
+        const {
+            target: {files},
+        } = event; 
+        const theFile = files[0];
+        const reader = new FileReader();
+        reader.onloadend = (finishedEvent) => {
+            const {
+                currentTarget: {result},
+            } = finishedEvent;
+            setAttachment(result);
+        };
+        reader.readAsDataURL(theFile);
+    }
+    const onClearAttachment = () => {
+        setAttachment(null);
+    }
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input value={mintweet} type="text" placeholder="What's on your min?" onChange={onChange} maxLength={120}/>
+                <input type="file" accept="image/*" onChange={onFileChange}/>
                 <input type="submit" value="Mintweet" />
+            {attachment && (
+                <div>
+                    <img src={attachment} width="50px" height="50px" alt="attachment"/>
+                    <button onClick={onClearAttachment}>Clear</button>
+                </div>
+            )}
+                
             </form>
             <div>
                 {mintweets.map( (mintweet) => (
