aa1d162c432ebf8d7061fc316a1024d4dc3dd153
mintchocoicecream <sxxhnjng@gmail.com>
2022-03-30 18:40:01 +0900

    Nav Home Icon

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -18,7 +18,7 @@ const Navigation = ({userObj}) => {
                 <ul className="navUl">
                     <li>
                         <Link to="/" className="navHome">
-                            <FontAwesomeIcon icon={faTwitter} size="2x" />
+                            <img src="https://firebasestorage.googleapis.com/v0/b/mintwitter-48f72.appspot.com/o/mintchoco01.png?alt=media&token=b9776a6c-23fd-4f20-aedf-03abaf16bf31" width="40px"/>
                             <span>
                                 Home
                             </span>
